59c26178524d7af39644c869b2e3b07832a91c78
Aliyah <emailkamu@example.com>
2026-06-25 10:29:16 +0700

    plspls

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,6 @@ return [
 
 'allowed_origins' => [
         'https://cosmetics-frontend-wfzx.vercel.app',
-        'https://cosmetics-frontend-wfzx-c8pp77fjv-aliyahs-projects-32011e54.vercel.app',
     ],
     'allowed_origins_patterns' => [],
 
