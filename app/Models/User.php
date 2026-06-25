2b99425a7502390e373f4e1db576a88f53bc1bfd
Aliyah <emailkamu@example.com>
2026-06-25 09:25:00 +0700

    c

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,23 +1,29 @@
 <?php
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // <-- PASTIKAN INI ADA
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable; // <-- WAJIB MASUKKAN HasApiTokens DI SINI
 
     protected $table = 'users';
 
     protected $fillable = [
-    'nama',
-    'username',
-    'email', // <--- Pastikan baris ini ada di sini!
-    'password',
-    'no_hp',
-    'alamat',
-];
+        'username',
+        'nama',
+        'email',
+        'password',
+        'hp',
+        'role',
+        'kode_unik',
+        'token',
+        'token_created_at'
+    ];
 
     protected $hidden = [
         'password',
