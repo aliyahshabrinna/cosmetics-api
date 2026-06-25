58202d13310cbd83613ba7d1560a332200c66676
Aliyah <emailkamu@example.com>
2026-06-25 11:02:02 +0700

    pls

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,15 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    // ANTIGRAVITY: Tulis domain Vercel barumu secara spesifik di sini (Tanpa Bintang)
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://cosmetics-frontend-ziq2.vercel.app', 
+        'https://cosmetics-frontend-ziq2.vercel.app',
+        'https://cosmetics-frontend-wfzx.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
@@ -27,6 +20,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true, // Wajib true karena berpasangan dengan domain spesifik di atas
-
+    'supports_credentials' => true, // Harus true dan berpasangan dengan domain di atas
 ];
